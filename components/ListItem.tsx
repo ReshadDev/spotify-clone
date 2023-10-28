23a63d2964de96f639ce50369fc52d6d61a44945
ReshadDev <reshadqarayev44@gmail.com>
2023-10-28 15:08:03 +0400

    final stage

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
@@ -12,16 +14,26 @@ interface ListItemProps {
 
 const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   const router = useRouter();
+  const { user } = useUser();
+  const authModal = useAuthModal();
 
   const onClick = () => {
-    // add auth before push
+    if (!user) {
+      return authModal.onOpen();
+    }
+
     router.push(href);
   };
 
   return (
     <button
       onClick={onClick}
-      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover: bg-neutral-100/20 pr-4 transition"
+      className="relative group flex items-center rounded-md overflow-hidden     gap-x-4 
+      bg-neutral-100/10 
+      cursor-pointer 
+      hover:bg-neutral-100/20 
+      transition 
+      pr-4"
     >
       <div className="relative min-h-[64px] min-w-[64px]">
         <Image className="object-cover" fill src={image} alt="like-icon" />
